5529b556119240232b9821fcb338c5060d3e6a73
snubeaver <kwonij2@snu.ac.kr>
2023-07-22 14:36:02 +0200

    implement deposit page without any data connection

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import '~/styles/index.css';
+import '~/configs/polyfill-wallet';
+
 import { Web3Modal } from '@web3modal/react';
 import ReactDOM from 'react-dom/client';
 import { WagmiConfig } from 'wagmi';
